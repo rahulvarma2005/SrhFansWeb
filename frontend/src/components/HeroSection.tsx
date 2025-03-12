99be0d3f5f0325edc9610a352a448dd3b273a3f4
Rahul <rahulmudunuri12@gmail.com>
2025-03-12 16:37:55 +0530

    path change

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/HeroSection.module.css';
 
 export default function HeroSection() {
   return (
-    <div className={styles.heroSection} id="home">
+    <div className={styles.heroSection} style={{background: "url('/srh-team2.png') no-repeat center/cover"}}id="home">
       <div className={styles.overlay}></div>
       <div className={styles.content}>
         <h1 className={styles.mainText}>Bleed Orange, Support SRH!</h1>
